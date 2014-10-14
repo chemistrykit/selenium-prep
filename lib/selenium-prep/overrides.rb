040c15215d6f1b189de54b3059dda3e6fffc0ded
Dave Haeffner <dhaeffner@gmail.com>
2014-10-14 17:41:15 -0400

    Added print to the overrides and cleaned up the debug env var check

--- lib/selenium-prep/overrides.rb
+++ lib/selenium-prep/overrides.rb
@@ -1,5 +1,19 @@
-module Kernel
-  def puts(input)
-    super input unless ENV['SE_DEBUG'] == 'off'
+module SeleniumPrep
+
+  def self.debug_off?
+    ENV['SE_DEBUG'] == 'off'
   end
+
+end
+
+module Kernel
+
+  def puts(input)
+    super input unless SeleniumPrep.debug_off?
+  end
+
+  def print(input)
+    super input unless SeleniumPrep.debug_off?
+  end
+
 end
